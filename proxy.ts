13d6b59615b1eafe83531a30a7f6dd201864f101
SerdiukSerhii <Serdyuk.logist@gmail.com>
2026-06-15 19:21:09 +0300

    fix parse option proxy

--- proxy.ts
+++ proxy.ts
@@ -4,11 +4,12 @@ import { checkSession } from './lib/api/serverApi';
 import { parse } from 'cookie';
 
 const privateRoutes = ['/profile', '/notes'];
-const publicRoutes = ['/sign-in', `/sign-up`];
+const publicRoutes = ['/sign-in', '/sign-up'];
 
 export async function proxy(request: NextRequest) {
   const { pathname } = request.nextUrl;
   const cookieStore = await cookies();
+
   const accessToken = cookieStore.get('accessToken')?.value;
   const refreshToken = cookieStore.get('refreshToken')?.value;
 
@@ -22,31 +23,25 @@ export async function proxy(request: NextRequest) {
 
       if (setCookie) {
         const cookieArray = Array.isArray(setCookie) ? setCookie : [setCookie];
+
         for (const cookieStr of cookieArray) {
           const parsed = parse(cookieStr);
-          const options = {
-            expires: parsed.Expires ? new Date(parsed.Expires) : undefined,
-            path: parsed.Path,
-            maxAge: Number(parsed['Max-Age']),
-          };
-          if (parsed.accessToken) cookieStore.set('accessToken', parsed.accessToken, options);
-          if (parsed.refreshToken) cookieStore.set('refreshToken', parsed.refreshToken, options);
+
+          if (parsed.accessToken) {
+            cookieStore.set('accessToken', parsed.accessToken);
+          }
+
+          if (parsed.refreshToken) {
+            cookieStore.set('refreshToken', parsed.refreshToken);
+          }
         }
 
         if (isPublicRoute) {
-          return NextResponse.redirect(new URL('/', request.url), {
-            headers: {
-              Cookie: cookieStore.toString(),
-            },
-          });
+          return NextResponse.redirect(new URL('/', request.url));
         }
 
         if (isPrivateRoute) {
-          return NextResponse.next({
-            headers: {
-              Cookie: cookieStore.toString(),
-            },
-          });
+          return NextResponse.next();
         }
       }
     }
@@ -70,5 +65,5 @@ export async function proxy(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/profile/:path*', '/notes/:path*', '/sign-in', `/sign-up`],
+  matcher: ['/profile/:path*', '/notes/:path*', '/sign-in', '/sign-up'],
 };
